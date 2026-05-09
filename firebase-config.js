4b7e6dfc866558894a9066f03a1261437f478862
越出つばさ <koshidetsubasa@koshidetsubasanoMacBook-Air.local>
2026-05-09 20:40:50 +0900

    Firebase設定追加

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB8ukokrPlYrOBlwV4oZY7D2XBH_fucs2s",
+  authDomain: "kakeibo-299f5.firebaseapp.com",
+  projectId: "kakeibo-299f5",
+  storageBucket: "kakeibo-299f5.firebasestorage.app",
+  messagingSenderId: "179598666653",
+  appId: "1:179598666653:web:00c6bfa532657e58b55d74"
+};
